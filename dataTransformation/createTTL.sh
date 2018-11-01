7960da1aada98adc1da1618a9d835b72d5c5a00a
Ivo Zandhuis <ivo@zandhuis.nl>
2018-11-01 16:53:58 +0100

    Better metadata.json

--- dataTransformation/createTTL.sh
+++ dataTransformation/createTTL.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -i -F "csv=@../data/p1223a_labels_semicolon_short.csv" -F "json=@../data/p1223a_labels_semicolon.csv-metadata.json" http://cattle.datalegend.net/convert -H'Accept: text/turtle' > ../data/constructs/p1223a_labels_semicolon_short.csv.ttl
+curl -i -F "csv=@../data/p1223a_labels_semicolon.csv" -F "json=@../data/p1223a_labels_semicolon.csv-metadata.json" http://cattle.datalegend.net/convert -H'Accept: text/turtle' > ../data/constructs/p1223a_labels_semicolon.csv.ttl
