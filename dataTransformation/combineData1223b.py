8165819c752c6c89d59236b29de146180ebe4384
Ivo Zandhuis <ivo@zandhuis.nl>
2019-02-18 15:50:21 +0100

    Adjusting scripting for HISCO in p1223b

--- dataTransformation/combineData1223b.py
+++ dataTransformation/combineData1223b.py
@@ -20,15 +20,17 @@ for index, row in deVriesData1884.iterrows():
 # merge standardizations
 ## occupations
 ### merge standard to dataframe
-#occupations     = pandas.read_csv(basedir + "data/hisco/bdv_occupations_hisco.csv")
-#deVriesData1884 = pandas.merge(deVriesData1884, occupations, how='left', left_on='berpmz', right_on='occupationalTitleDeVries')
+occupations     = pandas.read_csv(basedir + "data/hisco/p1223b_occupations_hisco.csv")
+deVriesData1884 = pandas.merge(deVriesData1884, occupations, how='left', left_on='berpmz', right_on='occupationalTitleDeVries')
 
 ### create triples
-#for index, row in deVriesData1884.iterrows():
-#   s = rdflib.URIRef("https://iisg.amsterdam/resource/bdv/" + str(row['volgnr1z']))
-#   p = rdflib.URIRef("http://purl.org/linked-data/sdmx/2009/dimension#occupation")
-#   o = rdflib.URIRef("https://iisg.amsterdam/hisco/code/hisco/" + str(row['hisco']))
-#   g.add((s,p,o))
+for index, row in deVriesData1884.iterrows():
+   hisco = str(row['hisco'])
+   if (hisco != "nan"):
+      s = rdflib.URIRef("https://iisg.amsterdam/resource/bdv/" + str(row['volgnr1z']))
+      p = rdflib.URIRef("http://purl.org/linked-data/sdmx/2009/dimension#occupation")
+      o = rdflib.URIRef("https://iisg.amsterdam/hisco/code/hisco/" + str(row['hisco']))
+      g.add((s,p,o))
 
 ## districts
 ### merge standard to dataframe
